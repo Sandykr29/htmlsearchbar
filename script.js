de2e0f568a32e9e8809a1b37ec709fa743643764
sandykr29 <sandeepkumar29774@yahoo.com>
2024-03-22 12:01:36 +0530

    scriptfile boxes capturing

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+const searchInput = document.getElementById('search-input');
+const clearButton = document.getElementById('clear-button');
+const suggestionsList = document.getElementById('suggestions-list');
+const dropdownContainer = document.getElementById('dropdown-container');
+
