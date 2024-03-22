a853d4da3c1a8c498002ca17c672790d94d63c37
sandykr29 <sandeepkumar29774@yahoo.com>
2024-03-22 12:09:45 +0530

    script-done

--- script.js
+++ script.js
@@ -13,4 +13,46 @@ fetch('data.json')
     countries = data;
   });
 
-
+// Function to clear
+function clearSearch() {
+    searchInput.value = '';
+    suggestionsList.innerHTML = '';
+    dropdownContainer.style.display = 'none';
+  }
+  
+  // handle search logic
+  function handleSearch() {
+    const searchText = searchInput.value.toLowerCase();
+    const filteredCountries = countries.filter(country => country.countryName.toLowerCase().includes(searchText));
+    displaySuggestions(filteredCountries);
+  }
+  
+  // display search
+  function displaySuggestions(suggestions) {
+    suggestionsList.innerHTML = '';
+    suggestions.slice(0, 5).forEach(suggestion => {
+      const li = document.createElement('li');
+      li.textContent = suggestion.countryName;
+      suggestionsList.appendChild(li);
+    });
+    dropdownContainer.style.display = suggestions.length > 0 ? 'block' : 'none';
+  }
+  
+  // Event-listeners
+  searchInput.addEventListener('input', () => {
+    debounce(handleSearch, 300)();
+  });
+  
+  clearButton.addEventListener('click', clearSearch);
+  
+  // Debounce function
+  function debounce(func, delay) {
+    let timeoutId;
+    return function() {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        func.apply(this, arguments);
+      }, delay);
+    };
+  }
+  
